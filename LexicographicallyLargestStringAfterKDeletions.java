9a43d24a263befa4d864a5f654bee2925ac18b4e
Sanskar Jain <jainsanskar721@gmail.com>
2025-06-24 23:21:51 +0530

    Lexicographically Largest String After K Deletions

---
+++ LexicographicallyLargestStringAfterKDeletions.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static String maxSubseq(String s, int k) {
+        int n = s.length();
+        int keep = n - k;  
+        StringBuilder stack = new StringBuilder();
+        
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            while (stack.length() > 0 && k > 0 && stack.charAt(stack.length() - 1) < c) {
+                stack.deleteCharAt(stack.length() - 1);
+                k--;
+            }
+            stack.append(c);
+        }
+        return stack.substring(0, keep);
+    }
+}

112a8cdcd36d6f6980ef76b5da1dc0a263f62e3a
Sanskar Jain <jainsanskar721@gmail.com>
2025-06-28 23:52:03 +0530

    Counting elements in two arrays

---
+++ Countingelementsintwoarrays.java
@@ -0,0 +1,28 @@
+class Solution {
+    public static ArrayList<Integer> countLessEq(int a[], int b[]) {
+        Arrays.sort(b);
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int val : a) {
+            int count = upperBound(b, val);
+            result.add(count);
+        }
+
+        return result;
+    }
+
+    private static int upperBound(int[] arr, int x) {
+        int left = 0, right = arr.length;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] <= x) {
+                left = mid + 1; 
+            } else {
+                right = mid;    
+            }
+        }
+        return left; 
+    }
+}

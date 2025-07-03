2f4da58225dd50873dd5f1067d74a018f9347dff
Sanskar Jain <jainsanskar721@gmail.com>
2025-07-04 00:01:42 +0530

    Longest Substring with K Uniques

---
+++ LongestSubstringwithKUniques.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int longestKSubstr(String s, int k) {
+        int n = s.length();
+        if (n == 0 || k == 0) return -1;
+
+        int maxLen = -1;
+        int left = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int right = 0; right < n; right++) {
+            char ch = s.charAt(right);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+            while (map.size() > k) {
+                char leftChar = s.charAt(left);
+                map.put(leftChar, map.get(leftChar) - 1);
+                if (map.get(leftChar) == 0) {
+                    map.remove(leftChar);
+                }
+                left++;
+            }
+
+            if (map.size() == k) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+        }
+
+        return maxLen;
+    }
+}

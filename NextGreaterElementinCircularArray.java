f28adc51d9ea04ca78e3b9f780340cdd55597887
Sanskar Jain <jainsanskar721@gmail.com>
2025-07-07 23:34:12 +0530

    Next Greater Element in Circular Array

---
+++ NextGreaterElementinCircularArray.java
@@ -0,0 +1,21 @@
+class Solution {
+    public ArrayList<Integer> nextLargerElement(int[] arr) {
+        int n = arr.length;
+        ArrayList<Integer> result = new ArrayList<>(Collections.nCopies(n, -1));
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            int index = i % n;
+            while (!stack.isEmpty() && arr[stack.peek()] <= arr[index]) {
+                stack.pop();
+            }
+            if (i < n) {
+                if (!stack.isEmpty()) {
+                    result.set(index, arr[stack.peek()]);
+                }
+            }
+            stack.push(index);
+        }
+
+        return result;
+    }
+}

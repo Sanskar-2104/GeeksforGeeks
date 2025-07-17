36e800b1d1c81bd821f7399fde8d72b1f20ea6fa
Sanskar Jain <jainsanskar721@gmail.com>
2025-07-17 23:30:26 +0530

    Power of k in factorial of n

---
+++ Powerofkinfactorialofn.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int maxKPower(int n, int k) {
+        Map<Integer, Integer> primeFactors = primeFactorization(k);
+        int result = Integer.MAX_VALUE;
+        for (Map.Entry<Integer, Integer> entry : primeFactors.entrySet()) {
+            int prime = entry.getKey();
+            int exponentInK = entry.getValue();
+            int countInFactorial = countPrimeInFactorial(n, prime);
+            result = Math.min(result, countInFactorial / exponentInK);
+        }
+
+        return result;
+    }
+
+    private int countPrimeInFactorial(int n, int p) {
+        int count = 0;
+        while (n > 0) {
+            n /= p;
+            count += n;
+        }
+        return count;
+    }
+
+    private Map<Integer, Integer> primeFactorization(int k) {
+        Map<Integer, Integer> factors = new HashMap<>();
+        for (int i = 2; i * i <= k; i++) {
+            while (k % i == 0) {
+                factors.put(i, factors.getOrDefault(i, 0) + 1);
+                k /= i;
+            }
+        }
+        if (k > 1) {
+            factors.put(k, 1);
+        }
+        return factors;
+    }
+}

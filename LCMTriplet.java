bc94ba1acfd0cc35cc5c3e821ffc177334eedebe
Sanskar Jain <jainsanskar721@gmail.com>
2025-07-19 00:01:13 +0530

    LCM Triplet

---
+++ LCMTriplet.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lcmTriplets(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        if (n == 3) return 6;
+
+        if (n % 2 != 0) {
+            return n * (n - 1) * (n - 2);
+        } else {
+            // If n is even
+            if (n % 3 != 0) {
+                return n * (n - 1) * (n - 3);
+            } else {
+                return (n - 1) * (n - 2) * (n - 3);
+            }
+        }
+    }
+}

6eba3c70b2582cbcf767e8ef8e935a48d89291f1
Sanskar Jain <jainsanskar721@gmail.com>
2025-07-24 23:48:54 +0530

    Last Moment Before All Ants Fall Out

---
+++ LastMomentBeforeAllAntsFallOut.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int getLastMoment(int n, int[] left, int[] right) {
+        int maxTime = 0;
+        for (int position : left) {
+            maxTime = Math.max(maxTime, position); 
+        }
+        for (int position : right) {
+            maxTime = Math.max(maxTime, n - position); 
+        }
+        
+        return maxTime;
+    }
+}

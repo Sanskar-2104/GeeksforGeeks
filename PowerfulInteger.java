86b721411a2a04cb7d6487d681c893f424322030
Sanskar Jain <jainsanskar721@gmail.com>
2025-07-31 23:44:53 +0530

    Powerful Integer

---
+++ PowerfulInteger.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int powerfulInteger(int[][] intervals, int k) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int[] interval : intervals) {
+            map.put(interval[0], map.getOrDefault(interval[0], 0) + 1);
+            map.put(interval[1] + 1, map.getOrDefault(interval[1] + 1, 0) - 1);
+        }
+
+        int currCount = 0;
+        int lastPosition = -1;
+        int maxPowerful = -1;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int point = entry.getKey();
+            int delta = entry.getValue();
+
+            if (currCount >= k && lastPosition != -1) {
+                maxPowerful = Math.max(maxPowerful, point - 1);
+            }
+
+            currCount += delta;
+            lastPosition = point;
+        }
+
+        return maxPowerful;
+    }
+}

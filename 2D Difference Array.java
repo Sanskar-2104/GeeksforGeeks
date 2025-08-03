affa2ad39f8c43dc4c8ab8f3ad90cb769481c531
Sanskar Jain <jainsanskar721@gmail.com>
2025-08-03 23:44:18 +0530

    2D Difference Array

---
+++ 2D Difference Array.java
@@ -0,0 +1,37 @@
+class Solution {
+    public ArrayList<ArrayList<Integer>> applyDiff2D(int[][] mat, int[][] opr) {
+        int n = mat.length;
+        int m = mat[0].length;
+        
+        int[][] diff = new int[n + 2][m + 2]; 
+        for (int[] op : opr) {
+            int val = op[0];
+            int r1 = op[1], c1 = op[2], r2 = op[3], c2 = op[4];
+
+            diff[r1][c1] += val;
+            diff[r1][c2 + 1] -= val;
+            diff[r2 + 1][c1] -= val;
+            diff[r2 + 1][c2 + 1] += val;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                diff[i][j] += diff[i][j - 1];
+            }
+        }
+        for (int j = 0; j < m; j++) {
+            for (int i = 1; i < n; i++) {
+                diff[i][j] += diff[i - 1][j];
+            }
+        }
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            ArrayList<Integer> row = new ArrayList<>();
+            for (int j = 0; j < m; j++) {
+                row.add(mat[i][j] + diff[i][j]);
+            }
+            result.add(row);
+        }
+
+        return result;
+    }
+}

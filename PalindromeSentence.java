02ac8b09c6455cddc1d049f0bf115bc6a67eec20
Sanskar Jain <jainsanskar721@gmail.com>
2025-08-05 23:54:12 +0530

    Palindrome Sentence

---
+++ PalindromeSentence.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalinSent(String s) {
+        StringBuilder cleaned = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            if (Character.isLetterOrDigit(ch)) {
+                cleaned.append(Character.toLowerCase(ch));
+            }
+        }
+        int left = 0;
+        int right = cleaned.length() - 1;
+        while (left < right) {
+            if (cleaned.charAt(left) != cleaned.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+}

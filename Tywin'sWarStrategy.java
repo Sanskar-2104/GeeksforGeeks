c940d206c32e63cf15b5f49daa46286585b657c0
Sanskar Jain <jainsanskar721@gmail.com>
2025-08-13 23:34:30 +0530

    Tywin's War Strategy

---
+++ Tywin'sWarStrategy.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minSoldiers(int[] arr, int k) {
+        int n = arr.length;
+        int neededLuckyTroops = (n + 1) / 2; 
+        int alreadyLucky = 0;
+        List<Integer> additions = new ArrayList<>();
+        
+        for (int soldiers : arr) {
+            if (soldiers % k == 0) {
+                alreadyLucky++;
+            } else {
+                additions.add(k - (soldiers % k));
+            }
+        }
+        
+        if (alreadyLucky >= neededLuckyTroops) return 0;
+        
+        Collections.sort(additions);
+        
+        int troopsToConvert = neededLuckyTroops - alreadyLucky;
+        int totalAdded = 0;
+        
+        for (int i = 0; i < troopsToConvert; i++) {
+            totalAdded += additions.get(i);
+        }
+        
+        return totalAdded;
+    }
+}

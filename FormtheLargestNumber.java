1bbac44653c6a064a3649631487b83496447fb8b
Sanskar Jain <jainsanskar721@gmail.com>
2025-08-16 23:33:53 +0530

    Form the Largest Number

---
+++ FormtheLargestNumber.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String findLargest(int[] arr) {
+        String[] nums = new String[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            nums[i] = String.valueOf(arr[i]);
+        }
+        Arrays.sort(nums, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                String ab = a + b;
+                String ba = b + a;
+                return ba.compareTo(ab);
+            }
+        });
+        if (nums[0].equals("0")) {
+            return "0";
+        }
+        StringBuilder result = new StringBuilder();
+        for (String num : nums) {
+            result.append(num);
+        }
+        
+        return result.toString();
+    }
+}

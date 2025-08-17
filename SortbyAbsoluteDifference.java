a5a74bcc10f1c20be81d614e6a1c8f4b09ea2fda
Sanskar Jain <jainsanskar721@gmail.com>
2025-08-17 23:54:07 +0530

    Sort by Absolute Difference

---
+++ SortbyAbsoluteDifference.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void rearrange(int[] arr, int x) {
+        List<int[]> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            list.add(new int[]{arr[i], i});
+        }
+        Collections.sort(list, (a, b) -> {
+            int diff1 = Math.abs(a[0] - x);
+            int diff2 = Math.abs(b[0] - x);
+            if (diff1 != diff2) {
+                return diff1 - diff2; 
+            } else {
+                return a[1] - b[1];  
+            }
+        });
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = list.get(i)[0];
+        }
+    }
+}
